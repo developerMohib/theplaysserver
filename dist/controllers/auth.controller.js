b56b1fd16e1bba0d07b7f9054c2f9b5ff933dfc6
developerMohib <mohibullahmohim2020@gmail.com>
2026-05-19 23:20:46 +0600

    fix:cookie setup for production level

--- dist/controllers/auth.controller.js
+++ dist/controllers/auth.controller.js
@@ -48,7 +48,7 @@ const register = (req, res, next) => __awaiter(void 0, void 0, void 0, function*
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
             maxAge: 7 * 24 * 60 * 60 * 1000,
         });
         res.status(201).json({
@@ -84,7 +84,7 @@ const login = (req, res, next) => __awaiter(void 0, void 0, void 0, function* ()
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: 'lax',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
             maxAge: 7 * 24 * 60 * 60 * 1000,
         });
         res.status(200).json({
--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -48,7 +48,7 @@ export const register = async (
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -93,7 +93,7 @@ export const login = async (
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'lax',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
